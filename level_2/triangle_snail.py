8bbbcc06662e2bc1cd0211767bd94b0ef88aa896
sujin16 <tnwls9712@ajou.ac.kr>
2021-01-05 19:10:41 +0900

    solution

--- level_2/triangle_snail.py
+++ level_2/triangle_snail.py
@@ -1,32 +1,17 @@
+from itertools import chain
 def soultion(n):
-    answer =[0]*sum([ i for i in range(1,n+1)])
-    count =1
-    index =0
+    maps = [[0 for _ in range(n)] for _ in range(n)]
+    y,x =-1,0
     value =1
-    num2=0
-    while n>0:
-        if count%3 ==1:
-            for i in range(0,n):
-                index+=i+num2
-                answer[index]= value
-                value+=1
-                
-        if count%3 ==2:
-            for i in range(0,n):
-                index+=1
-                answer[index] =value
-                value+=1
-                
-        if count %3 ==0:
-            for i in range(0,n):
-                index-= count+n-num2//2 -i -1
-                answer[index] =value
-                value+=1
-            num2+=2
-
-        count+=1
-        n-=1
-    return answer
-
-n =7
+    for i in range(n):
+        for j in range(i,n):
+            if i%3 ==0: y+=1
+            if i%3 ==1: x+=1
+            if i%3 ==2: y-=1;x-=1
+            
+            maps[y][x] =value
+            value+=1
+        
+    return [i for i in chain(*maps) if i !=0]
+n=5
 print(soultion(n))
