94cee4481744b099de05deb344261d973944f607
sujin16 <tnwls9712@ajou.ac.kr>
2021-01-08 13:50:25 +0900

    solve using greedy algorithm

--- level_2/make_bignumber.py
+++ level_2/make_bignumber.py
@@ -1,10 +1,25 @@
-from itertools import combinations
-
 def solution(number,K):
-    numbers= set(combinations(number,len(number)-K))
-    arr =[int("".join(x)) for x in numbers]
-    return str(max(arr))
-   
+    arr = list(number)
+    max_arr= sorted(arr,reverse=True)
+    answer=''
+    length = len(number)-K
+
+    while True:
+        for j in range(len(max_arr)):
+            if arr[0] >max_arr[j]:
+                answer+=str(arr[0])
+                arr= arr[1:]
+                break
+            
+            elif max_arr[j] in arr:
+                index =arr.index(max_arr[j]) 
+                if length -len(answer) <= len(arr)-index: 
+                    answer+=str(arr[index])
+                    arr= arr[index+1:]
+                    break    
+            
+        if len(answer) ==length: break
+    return answer
 
 number='1231234'
 K=3
