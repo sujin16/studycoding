e345a13bdc8f9c66f445907a7ce588b5130578b4
sujin16 <tnwls9712@ajou.ac.kr>
2021-01-09 17:03:36 +0900

    add comb

---
+++ level_2/camouflage.py
@@ -0,0 +1,29 @@
+from itertools import combinations
+from math import comb
+def solution(clothes):
+    closet ={}
+    for i in range(len(clothes)):
+        key = clothes[i][1] 
+        if key not in closet: closet[key] =0;closet[key]+=1
+        else: closet[key]+=1
+    answer =[]
+    for val in closet.values():
+        answer.append(val)
+    result =0
+    answer=[3]
+    for i in range(1,len(answer)+1):
+        arr= list(combinations(answer,i))
+        for a in arr:
+            num =1       
+            for b in a: num*=b
+            result+=num
+    
+    print(result)
+    return result
+
+
+clothes= [["yellow_hat","headgear"],
+["blue_sunglasses","eyewear"],
+["green_turban","headgear"]]
+
+print(solution(clothes))
